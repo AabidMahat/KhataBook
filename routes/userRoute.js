3c4a8573fb84daf4906f2735451e587095e5716d
Aabid Mahat <aabidmahat72@gmail.com>
2024-07-07 18:13:59 +0530

    Pre final Commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,12 +6,9 @@ const router = express.Router({ mergeParams: true });
 router.route("/createUser").post(userController.createNewUser);
 router.route("/login").post(userController.loginUser);
 
-router
-  .route("/logOut/:userId")
-  .post(userController.logOut)
+router.route("/logOut/:userId").post(userController.logOut);
 
-  .router.route("/getUser/:userId")
-  .get(userController.getUser);
+router.route("/getUser/:userId").get(userController.getUser);
 
 router.route("/updateUser/:userId").patch(userController.updateUser);
 
