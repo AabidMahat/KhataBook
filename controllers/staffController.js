060ca9c9fbbf651371ec593d1f6cd616b41333a0
Aabid Mahat <aabidmahat72@gmail.com>
2024-07-08 09:59:30 +0530

    Pre final Commit

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -50,9 +50,8 @@ exports.createNewStaff = async (req, res, next) => {
 
 exports.staffLogin = async (req, res, next) => {
   try {
-    const { staff_number, staffPassword } = req.body;
-    const staff = await Staff.findOne({ staff_number });
-
+    const { staff_number } = req.body;
+    const staff = await Staff.findOne({ staff_number }).populate("account_no");
     if (!staff) {
       return res.status(404).json({
         status: "Error",
