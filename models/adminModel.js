376c2dc6ec31b7a8d41ed4d5ab19ce315b949d3f
Aabid Mahat <aabidmahat72@gmail.com>
2024-08-12 18:03:42 +0530

    Final Commit

--- models/adminModel.js
+++ models/adminModel.js
@@ -12,6 +12,13 @@ const adminSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minLength: [8, "Password must be at least 8 characters long"],
   },
+  amount: {
+    type: Number,
+    default: 99,
+  },
+  upiId: {
+    type: String,
+  },
 });
 
 const Admin = mongoose.model("Admin", adminSchema);
